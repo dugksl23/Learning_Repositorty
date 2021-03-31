3c75e3fd54a046041081e8e3a20e608d328c7271
kkm8257 <tenman2002@gmail.com>
2021-03-31 18:33:42 +0900

    김경민 - login 다시 수정

--- src/main/java/ai/plats/domain/board/web/WritingController.java
+++ src/main/java/ai/plats/domain/board/web/WritingController.java
@@ -7,6 +7,7 @@ import ai.plats.domain.board.service.WritingService;
 import ai.plats.domain.user.entity.User;
 import ai.plats.domain.user.service.UserJoinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.security.Principal;
 import java.util.Optional;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class WritingController {
     @Autowired
     WritingService writingService;
 
