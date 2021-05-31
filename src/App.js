7c28e3358a7729445fdd3239cd94b2f6c4a55854
dugksl23 <dugksl23@gmail.com>
2021-05-31 17:42:10 +0900

    app component(controller/model) and component props(views) done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import "./App.css";
 import React, { Component } from "react";
 
-// 1. 서브젝트 component 생성
-class Subject extends Component {
-  // js는 function 선언시, function keyword가 필요하다.
-  // 다만, class의 멤버메소드에서는 ec6부터 생략 가능.
-  render() {
-    return (
-      // react의 컴포넌트의 특징 : 하나의 최상위 태그로 시작한다.
-      <header>
-        <h1>WEB</h1>
-        hellow world
-      </header>
-    );
-  }
-}
-
 class TOC extends Component {
   render() {
     return (
@@ -34,9 +19,24 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is ~~
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
+    );
+  }
+}
+
+// 1. 서브젝트 component 생성
+class Subject extends Component {
+  // js는 function 선언시, function keyword가 필요하다.
+  // 다만, class의 멤버메소드에서는 ec6부터 생략 가능.
+  render() {
+    return (
+      // react의 컴포넌트의 특징 : 하나의 최상위 태그로 시작한다.
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
     );
   }
 }
@@ -46,9 +46,10 @@ class App extends Component {
     return (
       // div className>태그 안에 태그를 생성한다. => SPA
       <div className="App">
-        <Subject></Subject>
+        <Subject title="HTML" sub="hellow wrold"></Subject>
+        <Subject title="React" sub="hellow react"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText MarkUp"></Content>
       </div>
     );
   }
