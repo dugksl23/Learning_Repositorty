480135b7bb6dbf2f99181245976351cf6c942a8d
dugksl23 <dugksl23@gmail.com>
2021-06-01 11:59:55 +0900

    동적 event 바인딩 및 event 방지 함수 사용

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -9,7 +9,18 @@ class Subject extends Component {
       // react의 컴포넌트의 특징 : 하나의 최상위 태그로 시작한다.
       <header>
         <h1>
-          <a href="/">{this.props.title}</a>
+          <a
+            href="/"
+            onClick={(e) => {
+              console.log(e);
+              e.preventDefault();
+              //debugger;
+              // 이벤트가 주입이 되고, e 변수에 할당된 이벤트를 핸들링할 수 있도록 주입된 정보이다.
+              // e.preventDefault(); // 이벤트가 할당된 태그의 원래 기능을 하지 못하도록 하는 함수이다.
+            }}
+          >
+            {this.props.title}
+          </a>
         </h1>
         {this.props.sub}
       </header>
