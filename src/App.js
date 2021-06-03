63cddee6127143710d0fb965abc41336b21673fa
dugksl23 <godkor200@gmail.com>
2021-06-03 19:37:08 +0900

    useStatus() 완료 및 입력값에 대하여 useState에 값을 update 할 떄에도 반 초기값을  한다.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ const App = () => {
   const [personInfo, setPersonInfo] = useState({ name: "요한", age: 20 });
   const [learning, setLearning] = useState(["node.js"]);
 
+  const [inputData, setInputData] = useState([]); // useState도 초기값을 주어야 한다.
+  const changeInputData = (e) => {
+    e.preventDefault();
+    setInputData([e.target.text.value]);
+  };
+
   return (
     <>
       <h1> toDo Application 향상</h1>
-      <form action="/">
+      <form onSubmit={changeInputData} action="/">
         <input type="text" name="text" placeholder="text"></input>
         <button type="submit">할일 추가</button>
       </form>
 
-      <List personInfo={personInfo} learning={learning}></List>
+      <List
+        personInfo={personInfo}
+        learning={learning}
+        inputData={inputData}
+      ></List>
     </>
   );
 };
--- src/components/ListHook.js
+++ src/components/ListHook.js
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
+
 const List = (props) => {
   let { personInfo } = props; // 구조 분해 할당
   let { learning } = props; // 구조 분해 할당
+  let { inputData } = props;
 
-  const personsInfo = Object.entries(personInfo);
+  const personsInfo = Object.entries(personInfo).map((info) => {
+    return <li>{info}</li>;
+  });
   const toLearning = learning.map((toDo) => {
     return <li>{toDo}</li>;
+  });
+
+  const input = inputData.map((datas) => {
+    console.log(datas);
+    return <li>{datas}</li>;
+  });
+
+  useEffect(() => {
+    console.log("useEffect");
   });
 
   return (
     <ul>
       {personsInfo}
       {toLearning}
+      {input}
     </ul>
   );
 };
